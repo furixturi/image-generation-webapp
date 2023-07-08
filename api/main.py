d258836e3d8b4118d5970784176424ec5a7c20e2
Xiaoli Shen <alabebop@gmail.com>
2023-07-08 19:15:14 +0900

    process and save image in API folder

--- api/main.py
+++ api/main.py
@@ -2,6 +2,8 @@ from typing import Union
 
 from fastapi import FastAPI
 
+import utils
+
 app = FastAPI()
 
 
@@ -13,3 +15,10 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/generate-image")
+def generate_image(prompt: str):
+    image, prmpt = utils.parse_response(utils.query_endpoint(prompt))
+    utils.save_image(image)
+    return {"prompt": prmpt}
--- api/utils.py
+++ api/utils.py
@@ -1,12 +1,13 @@
 import boto3
 import json
+from PIL import Image
+import numpy as np
 
 endpoint_name = "jumpstart-dft-stable-diffusion-v2-1-base"
+client = boto3.client("runtime.sagemaker")
 
 
 def query_endpoint(text):
-    client = boto3.client("runtime.sagemaker")
-
     encoded_text = text.encode("utf-8")
     response = client.invoke_endpoint(
         EndpointName=endpoint_name,
@@ -21,3 +22,9 @@ def query_endpoint(text):
 def parse_response(query_response):
     response_dict = json.loads(query_response["Body"].read())
     return response_dict["generated_image"], response_dict["prompt"]
+
+
+def save_image(pixels):
+    arr = np.array(pixels, dtype=np.uint8)
+    img = Image.fromarray(arr)
+    img.save("new.png")
